ff3d9aa443f0f121bf4a1cfa331c2dd75ad22239
Amit Wolfenfeld <amitw@traxretail.com>
2017-09-18 15:08:04 +0300

    added port number

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=80)
